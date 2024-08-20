0c59ac785028a6b2ffc1d79a7bb2ecd555965981
orawczak <pavel.orawczak@seznam.cz>
2024-08-20 13:00:51 +0200

    fix sg

--- main.tf
+++ main.tf
@@ -36,11 +36,11 @@ module "blog_sg" {
 
   vpc_id = data.aws_vpc.default.id
 
-  ingres_rules = ["http-80-tcp", "https-443-tcp"]
+  ingress_rules = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
 
-  eggres_rules = ["all-all"]
-  eggress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
 }
 
 resource "aws_security_group" "blog" {
