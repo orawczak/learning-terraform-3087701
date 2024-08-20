4007d437f2c795638a6c5e081b0bb33ea273291d
orawczak <pavel.orawczak@seznam.cz>
2024-08-20 15:30:30 +0200

    as10

--- main.tf
+++ main.tf
@@ -70,7 +70,7 @@ module "blog_alb" {
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"      
-      target_id = 
+      target_id = autoscaling.image_id
     }
   }
 
